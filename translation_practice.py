2d2ea6680d5e9d8f753fe556afd479435734be49
Connor Fairman <connorfairman23@gmail.com>
2017-11-28 16:11:59 -0500

    added new functionality
    Now we can take in a file, save its contents as a string, and then use that string as an argument in the translation function

--- translation_practice.py
+++ translation_practice.py
@@ -6,12 +6,16 @@ Created on Nov 28, 2017
 
 from googletrans import Translator
 
-translator = Translator()
-translation = translator.translate("We are gonna win so big that we are going to get bored with winning", 'es', 'en')
-print(translation.text)
+file_object = open("source-tweet.txt", "r")
+tweet = file_object.read()
 
-file_object = open("translation.txt", "w")
-file_object.truncate(0)
-file_object.write(translation.text)
+translator = Translator()
+translation = translator.translate(tweet, 'es', 'en')
+print(translation.text)
 file_object.close()
+
+file_object1 = open("translation.txt", "w")
+file_object1.truncate(0)
+file_object1.write(translation.text)
+file_object1.close()
 
