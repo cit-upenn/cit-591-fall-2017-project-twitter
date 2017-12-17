e8f0fd35e049bbdb7422c59a5b265289bf5a7a16
connersevans <33667678+connersevans@users.noreply.github.com>
2017-12-17 13:22:32 -0500

    Add files via upload
    Python version of implemented Twitter API

---
+++ Secret.py
@@ -0,0 +1,29 @@
+'''
+Created on Dec 17, 2017
+
+@author: connerevans
+'''
+
+
+
+#class Secret(object):
+consumer_key = 'KMW0enDSiGQGzZEQKtBHEwGVa'
+consumer_secret = 'BP0XI3EubKSWMqFeSfc26Ygddck7cQJ7u8gd1Ixuyelw1FBlbT'
+access_token = '223724013-Ch206Es5QsoG6ST1gBycnYaanSVtjUufEDbgcJgZ'
+access_token_secret = 'DF9UMHhvpU8oCQHgGUCHxIxSW4DLB5bdm5lYSQFdVg05I'
+   
+#def get_key():
+#   consumer_key = 'KMW0enDSiGQGzZEQKtBHEwGVa'
+#  return consumer_key
+    
+#def get_secret():
+#    consumer_secret = 'BP0XI3EubKSWMqFeSfc26Ygddck7cQJ7u8gd1Ixuyelw1FBlbT'
+#    return consumer_secret
+    
+#def get_token():
+#    access_token = '223724013-Ch206Es5QsoG6ST1gBycnYaanSVtjUufEDbgcJgZ'
+#    return access_token
+    
+#def get_token_secret():
+#    access_token_secret = 'DF9UMHhvpU8oCQHgGUCHxIxSW4DLB5bdm5lYSQFdVg05I'
+#    return access_token_secret
