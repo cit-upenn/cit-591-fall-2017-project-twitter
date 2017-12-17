71825dde08269d277ac39056d153f55f9e2ee796
Connor Fairman <connorfairman@Connors-MBP.hotspot>
2017-12-17 11:10:09 -0500

    defined function for translation

--- src/translation_practice.py
+++ src/translation_practice.py
@@ -5,21 +5,25 @@ Created on Nov 28, 2017
 '''
 from googletrans import Translator
 
-file_object = open("source-tweet.txt", "r")
-tweet = file_object.read()
 
-translator = Translator()
-translation = translator.translate(tweet, 'es', 'en')
-print(translation.text)
-file_object.close()
+def translateTextFile():
+    file_object = open("source-tweet.txt", "r")
+    tweet = file_object.read()
+    translator = Translator()
+    translation = translator.translate(tweet, 'es', 'en')
+    print(translation.text)
+    file_object.close() 
+    file_object1 = open("translation.txt", "w")
+    file_object1.truncate(0)
+    file_object1.write(translation.text)
+    file_object1.close()
 
-file_object1 = open("translation.txt", "w")
-file_object1.truncate(0)
-file_object1.write(translation.text)
-file_object1.close()
-
+translateTextFile()
 
 
 # Instead of watching a file, create a new text file every time you take in a tweet
+# Combine our parts into one module to get it to run together
+# Look up Python testing
+# structure code above into unique functions
 ######################################################################################
 
